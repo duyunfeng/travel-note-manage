c19082f8e483afd4956f054c283f95086afc138f
duyunfeng <944588913@qq.com>
2024-09-18 17:45:52 +0800

    新增登录接口

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 const Scheam = mongoose.Schema;
 const scheamList = {
     'user': new Scheam({
@@ -8,27 +8,69 @@ const scheamList = {
         'password': String
     })
 }
+
+const app = express();
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST"); // 允许的方法
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const getModel = (name) => {
     if(scheamList[name]) {
         return mongoose.model(name, scheamList[name]);
     }
 }
+app.get('/', (req,res) => {
+    res.json({
+        'message': 'Welcome to ExpressJS'
+    })
+})
+
+app.options('*', (req, res) => {
+    res.sendStatus(204);
+});
+
+const returnRes = (res, code, message, data) => {
+    resData = {
+        code,
+        message,
+    }
+    if(data) {
+        resData.data = data;
+    }
+    res.status(code).json(resData);
+}
+app.post('/login', (req, res) => {
+    let code = 200;
+    const User = getModel('user');
+    const {username, password} = req.body;
+    User.findOne({username: username})
+    .then(user => {
+        if(user) {
+            if(user.password === password) {
+                returnRes(res, code, '登录成功', user)
+            } else {
+                code = 500;
+                returnRes(res, code, '密码错误')
+            }
+        } else {
+            code = 500;
+            returnRes(res, code, '用户不存在')
+        }
+    }).catch((err) => {
+        code = 500;
+        returnRes(res, code, '数据库错误')
+    });
+})
 mongoose.connect('mongodb://localhost:27017/myDataBase')
 .then(() => {
-
+    app.listen(3000,() => {
+        console.log('Server is running on port 3000');
+    });
     console.log('Connected to MongoDB');
 }).catch(err => {
     cachesonsole.log('Error connecting to MongoDB', err);
 });
-
-mongoose.connection.once('open', async () => {
-    const User = getModel('user');
-    const user = await User.findOne({username: 'admin'});
-    console.log(user)
-    if(!user) {
-        User.create({username: 'admin', password: '123456'}).then(async res => {
-            const user = await User.findOne({username: 'admin'});
-            console.log(user)
-        })
-    } 
-})
