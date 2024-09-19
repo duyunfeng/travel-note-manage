d432a4118cba0dabca92be60ab88acf3f39e8cbb
duyunfeng <944588913@qq.com>
2024-09-19 23:44:53 +0800

    修改监听端口

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ for(let path in router) {
 console.log('Routers loaded successfully.');
 mongoose.connect('mongodb://localhost:27017/myDataBase')
 .then(() => {
-    app.listen(3000,() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT,() => {
         console.log('Server is running on port 3000');
     });
     console.log('Connected to MongoDB');
